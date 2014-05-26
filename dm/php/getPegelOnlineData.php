d517b38c089727445c24bdf1a88909d16520c122
Dominik <schlomm.darmann@googlemail.com>
2014-05-26 19:03:10 +0200

    Highchart depending on station
    Change from universal BONN-chart, which was previously used for demo, to
    the corresponding chart for each station

--- dm/php/getPegelOnlineData.php
+++ dm/php/getPegelOnlineData.php
@@ -1,9 +1,13 @@
 <?php
-
 // It reads the values from the last 24hours of a measurement station
 // It reads a json formatted text file and outputs it.
+// Passes the measurementStationName from map.js
+$measurementStationName = $_GET["measurementStationName"];
+// Decode URL for links with space
+$id = urldecode($measurementStationName);
+$id = str_replace(" ", "%20", $id);
 
-$string = file_get_contents("http://www.pegelonline.wsv.de/webservices/rest-api/v2/stations/BONN/W/measurements.json?start=P1D");
+$string = file_get_contents("http://www.pegelonline.wsv.de/webservices/rest-api/v2/stations/".$id."/W/measurements.json?start=P1D");
 echo $string;
 
 ?>
