57656291b811e005074755f204d4e79ebd6919a0
Dominik <schlomm.darmann@googlemail.com>
2014-05-26 19:06:21 +0200

    Highcharts depending on station ([id] is used)

--- dm/js/highchartPegelOnline.js
+++ dm/js/highchartPegelOnline.js
@@ -1,24 +1,23 @@
-$(function () {
+initHighChartForStation = function (id) {
     var options = {
         chart: {
-            renderTo: 'popup-content',
-            type: 'line',
-			width: 780,
+            renderTo: 'highchartContainer',
+            type: 'spline',
             shadow: true
         },
         title: {
-            text: 'Water level of the last xx days'
+            text: 'Water level of the last 12 hours'
         },
         subtitle: {
-            text: 'Measured in xxxx'
+            text: 'Measured in '+decodeURIComponent(id)
         },
         credits: {
-            text: 'Data',
-            href: 'link'
+            text: 'Source',
+            href: 'http://www.pegelonline.wsv.de'
         },
         xAxis: {
             type: 'datetime',
-            minRange: 1 * 24 * 3600000 // one day
+            minRange: 0.5 * 24 * 3600000 // half day
         },
         yAxis: {
             title: {
@@ -30,7 +29,7 @@ $(function () {
         },
         series: [{}]
     };
-    $.getJSON('php/getPegelOnlineData.php', function (data) {
+    $.getJSON('php/getPegelOnlineData.php?measurementStationName='+id, function (data) {
         chart_data = [];
         $.each(data, function (i, obj) {
             chart_data.push([Date.parse(obj.timestamp), obj.value]);
@@ -38,4 +37,4 @@ $(function () {
         options.series[0].data = chart_data;
         var chart = new Highcharts.Chart(options);
     });
-});
+};
