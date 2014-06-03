a43f4e40d70e3fb6102de9378c8eee460e984898
Dominik <schlomm.darmann@googlemail.com>
2014-06-03 21:38:44 +0200

    Some minor improvments
    Added a crosshair to get a overview about both datasets at once; changed
    the visualization of data values; changed "Temperatur" to "Temperature"
    by proving the right language  (check last part of URL)

--- dm/js/highchartWeatherForecast.js
+++ dm/js/highchartWeatherForecast.js
@@ -1,56 +1,70 @@
 initHighChartForForecast = function (latlng) {
 	var options = {
-		chart: {
-			renderTo: 'popup-content',
-		type:'spline',
-			width: 780,
-			shadow: false
+		chart : {
+			renderTo : 'popup-content',
+			type : 'spline',
+			width : 780,
+			shadow : false
 		},
-		title: {
-			text: 'Weatherforecast for the next seven days'
+		title : {
+			text : 'Weatherforecast for the next seven days'
 		},
-		subtitle: {
-			text: ''
+		subtitle : {
+			text : ''
 		},
-		credits: {
-			text: 'Source',
-			href: 'openweathermap.com'
+		credits : {
+			text : 'Source',
+			href : 'openweathermap.com'
 		},
-		xAxis: {
-			type: 'datetime',
-			plotLines: [{dashStyle: 'longdashdot'}]
-			
-		   
-		},	
-		yAxis: [{
-			title: {
-				text: 'Temperature in \xB0C'
-			},
+		xAxis : {
+			type : 'datetime',
+			plotLines : [{
+					dashStyle : 'longdashdot'
+				}
+			]
+
 		},
-		{
-			title : {
-				text: 'Presure'
-			},
-			opposite: true,
-			
-		}],
-			
-		plotOptions: {
-			series: {
-				pointStart: '',
-				pointInterval: 18 * 3600 * 1000 // ~seven day
+		yAxis : [{
+				title : {
+					text : 'Temperature in \xB0C'
+				},
+			}, {
+				title : {
+					text : 'Pressure'
+				},
+				opposite : true,
+
+			}
+		],
+
+		plotOptions : {
+			series : {
+				pointStart : '',
+				pointInterval : 18 * 3600 * 1000 // ~seven day
 			}
 		},
-					   
-		legend: {
-			enabled: false
+		tooltip : {
+			crosshairs : true,
+			shared : true
 		},
-		series: [{},{}]
+		legend : {
+			enabled : true
+		},
+		series : [{
+				marker : {
+					symbol : 'square'
+				},
+			}, {
+				marker : {
+					symbol : 'diamond'
+				},
+			}
+		]
 	}
-	$.getJSON('http://api.openweathermap.org/data/2.5/forecast/daily?lat='+latlng.lat+'&lon='+latlng.lng+'&cnt=10&mode=json', function (data) {
-		chart_data = [[],[]];
-		options['plotOptions']['series']['pointStart'] =  data.list[0].dt*1000;
-		options['subtitle']['text'] = data.city.name;
+	$.getJSON('http://api.openweathermap.org/data/2.5/forecast/daily?lat=' + latlng.lat + '&lon=' + latlng.lng + '&cnt=10&mode=json&lang=en', function (data) {
+		chart_data = [[], []];
+		options['plotOptions']['series']['pointStart'] = data.list[0].dt * 1000;
+		options['subtitle']['text'] = "around "+data.city.name;
 		for (var i = 0; i < data.list.length; i++) {
 			chart_data[0].push(parseFloat((data.list[i].temp.day - 272.15).toFixed(2)));
 			chart_data[1].push(data.list[i].pressure);
@@ -58,12 +72,12 @@ initHighChartForForecast = function (latlng) {
 		options.series[0]['data'] = chart_data[0];
 		options.series[0]['color'] = '#BF0B23';
 		options.series[0].yAxis = 0;
-		options.series[0].name = 'Temperatur in \xB0C';
-		
+		options.series[0].name = 'Temperature in \xB0C';
+
 		options.series[1]['data'] = chart_data[1];
 		options.series[1].yAxis = 1;
 		options.series[1].name = 'Pressure in hPa';
-		
+
 		var chart = new Highcharts.Chart(options);
 	});
 };
