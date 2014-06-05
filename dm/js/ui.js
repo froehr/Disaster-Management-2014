44dda5bd14234a9bac2089fd91c7439c613069de
froehr <f.roehr@uni-muenster.de>
2014-06-05 16:09:08 +0200

    First try with Intro.JS

--- dm/js/ui.js
+++ dm/js/ui.js
@@ -148,7 +148,28 @@ $('#weatherforcast').click(function() {
 });
 
 $('#help').click(function() {
-	createPopUp(800, 300, '');
+	var intro = introJs();
+	intro.setOption('doneLabel', 'Exit');
+	intro.setOption('nextLabel', 'Next');
+	intro.setOption('showButtons', 'true');
+	intro.setOption('showStepNumbers', 'false');
+	
+	intro.setOptions({
+	  steps: [
+	    {
+	      element: '#emergency',
+	      intro: 'With this Buttons you can add a new Emergency',
+	      exitOnEsc: 'false'
+	    },
+	    {
+	      element: '#need-support',
+	      intro: 'With this Buttons you call for support',
+	      exitOnEsc: 'false'
+	    },
+	  ]
+	});
+
+      intro.start();	
 });
 
 $('#about').click(function() {
