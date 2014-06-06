0802cfbc05f64f3df3b7eda7f7096e14ae26b0da
Markus <mkonkol@arcor.de>
2014-06-06 14:25:13 +0200

    database plus alerts
    saves messages into the database and triggers alert messages if wrong
    information is submitted

--- dm/js/db_connection.js
+++ dm/js/db_connection.js
@@ -5,54 +5,215 @@
 
 			if (type === 'marker') {				
 				coordinates = JSON.stringify(layer.toGeoJSON());
-				//var coord=coordinates.geometry.coordinates.toString();
-				console.log(coordinates);
+				//console.log(coordinates);
 			}
 			 
-			if (type === 'polyline') {
+			else if (type === 'polyline') {
 				coordinates = JSON.stringify(layer.toGeoJSON());
-				console.log(coordinates);
+				//console.log(coordinates);
 			}
 			
-			if (type === 'polygon') {
+			else if (type === 'polygon') {
 				coordinates = JSON.stringify(layer.toGeoJSON());
-				console.log(coordinates);	
+				//console.log(coordinates);	
 			}
 			
-			if (type === 'rectangle') {
+			else if (type === 'rectangle') {
 				coordinates = JSON.stringify(layer.toGeoJSON());
-				console.log(coordinates);
+				//console.log(coordinates);
+			}
+			else{
+				coordinates = "";
 			}
 		});
 
 	
 	function saveToDB(){
 		d = new Date()
-		console.log(d.getMonth()+1);
-		/*if (isNaN(document.getElementById("people_attending").value) || isNaN(document.getElementById("people_attending").value)){
-			alert("People attending and people need is not a number");
-		}*/
-		//else{
+
+		var type = document.getElementById("issue").value;
+		switch (type){
+			case "emergency":
+				submitEmergency();
+				break;
+			case "need-support":
+				submitEmergency();
+				break;
+			case "offer-support":
+				submitOfferSupport();
+				break;
+			case "message":
+				submitMessage();
+				break;
+			default:
+				break;
+		}
+	}	
+	
+	function submitEmergency(){
+		
+		var issue = document.getElementById("issue").value;
+		var title = document.getElementById("title").value;
+		var description = document.getElementById("description").value;
+		var category = document.getElementById("category").value;
+		var contact = document.getElementById("person_contact").value;
+		var geometry = coordinates;
+		var name = document.getElementById("person_name").value;
+		var peopleAttending = document.getElementById("people_attending").value;
+		var peopleNeeded = document.getElementById("people_need").value;
+		var tags = document.getElementById("tags").value;
+		var creationDate = d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+		
+		if (peopleAttending == "" && peopleAttending == ""){
+			peopleAttending = 0;
+			peopleNeeded = 0;
+		}
+		else{
+		}
+		
+		if (isNaN(document.getElementById("people_attending").value) || isNaN(document.getElementById("people_attending").value)){
+			alert("Helpers attending/needed is not a number");
+		}
+		else if (document.getElementById("title").value == "" || document.getElementById("description").value == ""){
+			alert("Title and description are still empty");
+		}
+		else if (coordinates == ""){
+			alert("A Feature on the map is missing");
+		}
+		else if (document.getElementById("person_contact").value == ""){
+			alert("Contact information missing");
+		}
+		else if ((document.getElementById("people_attending").value != "" && document.getElementById("people_need").value =="") || 
+				(document.getElementById("people_need").value != "" && document.getElementById("people_attending").value == "")){
+			alert("People attending or people needed missing");
+		}
+		else{
 			$.post(
 				"php/insertMessage.php?",
 				{	
-				Issue:document.getElementById("issue").value,
-				Title:document.getElementById("title").value,
-				Geometry: coordinates,
-				Description:document.getElementById("description").value,
-				Startdate:document.getElementById("startdate").value + " " + document.getElementById("starttime").value,
-				Enddate:document.getElementById("enddate").value + " " + document.getElementById("endtime").value,
-				CreationDate: d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds(),
-				Category:document.getElementById("category").value,
-				PersonContact:document.getElementById("person_contact").value,
-				Name:document.getElementById("person_name").value,
-				PeopleAttending:document.getElementById("people_attending").value,
-				PeopleNeeded:document.getElementById("people_need").value,
-				Tags:document.getElementById("tags").value,
+				Issue:issue,
+				Title:title,
+				Description:description,
+				Category:category,
+				PersonContact:contact,
+				Geometry: geometry,
+				Name:name,
+				PeopleAttending:peopleAttending,
+				PeopleNeeded:peopleNeeded,
+				Tags:tags,
+				CreationDate: creationDate
 				},
 				function(data){console.log(data);}	
 				);
-		//}	
-}
+		}	
+	}
+	
+	function submitOfferSupport(){
+	
+		var issue = document.getElementById("issue").value;
+		var title = document.getElementById("title").value;
+		var description = document.getElementById("description").value;
+		var category = document.getElementById("category").value;
+		var contact = document.getElementById("person_contact").value;
+		var geometry = coordinates;
+		var name = document.getElementById("person_name").value;
+		var peopleAttending = document.getElementById("people_attending").value;
+		var peopleNeeded = document.getElementById("people_need").value;
+		var tags = document.getElementById("tags").value;
+		var creationDate = d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+		
+		if (peopleAttending == "" && peopleAttending == ""){
+			peopleAttending = 0;
+			peopleNeeded = 0;
+		}
+		else{
+		}
+		
+		if (isNaN(document.getElementById("people_attending").value) || isNaN(document.getElementById("people_attending").value)){
+			alert("Helpers attending/needed is not a number");
+		}
+		else if (document.getElementById("title").value == "" || document.getElementById("description").value == ""){
+			alert("Title and description are still empty");
+		}
+		else if (document.getElementById("person_contact").value == ""){
+			alert("Contact information missing");
+		}
+		else if ((document.getElementById("people_attending").value != "" && document.getElementById("people_need").value =="") || 
+				(document.getElementById("people_need").value != "" && document.getElementById("people_attending").value == "")){
+			alert("People attending or people needed missing");
+		}
+		else{
+			$.post(
+				"php/insertMessage.php?",
+				{	
+				Issue:issue,
+				Title:title,
+				Description:description,
+				Category:category,
+				PersonContact:contact,
+				Geometry: geometry,
+				Name:name,
+				PeopleAttending:peopleAttending,
+				PeopleNeeded:peopleNeeded,
+				Tags:tags,
+				CreationDate: creationDate
+				},
+				function(data){console.log(data);}	
+				);
+		}	
+	}
+	
+	function submitMessage(){
+	
+		var issue = document.getElementById("issue").value;
+		var title = document.getElementById("title").value;
+		var description = document.getElementById("description").value;
+		var category = document.getElementById("category").value;
+		var contact = document.getElementById("person_contact").value;
+		var geometry = coordinates;
+		var name = document.getElementById("person_name").value;
+		var peopleAttending = document.getElementById("people_attending").value;
+		var peopleNeeded = document.getElementById("people_need").value;
+		var tags = document.getElementById("tags").value;
+		var creationDate = d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+		
+		if (peopleAttending == "" && peopleAttending == ""){
+			peopleAttending = 0;
+			peopleNeeded = 0;
+		}
+		else{
+		}
+		
+		if (isNaN(peopleAttending) || isNaN(peopleNeeded)){
+			alert("Helpers attending/needed is not a number");
+		}
+		else if (document.getElementById("title").value == "" || document.getElementById("description").value == ""){
+			alert("Title and description are still empty");
+		}
+		else if ((document.getElementById("people_attending").value != "" && document.getElementById("people_need").value =="") || 
+				(document.getElementById("people_need").value != "" && document.getElementById("people_attending").value == "")){
+			alert("People attending or people needed missing");
+		}
+		else{
+			$.post(
+				"php/insertMessage.php?",
+				{	
+				Issue:issue,
+				Title:title,
+				Description:description,
+				Category:category,
+				PersonContact:contact,
+				Geometry: geometry,
+				Name:name,
+				PeopleAttending:peopleAttending,
+				PeopleNeeded:peopleNeeded,
+				Tags:tags,
+				CreationDate: creationDate
+				},
+				function(data){console.log(data);}	
+				);
+		}	
+	}
+	
 	
 	
