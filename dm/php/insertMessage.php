ef0a1786fb669e15c31c0882281b15b1a5995c22
Dominik <schlomm.darmann@gmail.com>
2014-06-20 15:02:41 +0200

    Added logger for message inserting
    By sending messages, the message is now logged as a json.

--- dm/php/insertMessage.php
+++ dm/php/insertMessage.php
@@ -2,29 +2,42 @@
 $dbconn = pg_connect("host=host port=5432 dbname=dbname user=user password=pw")
     or die('Verbindungsaufbau fehlgeschlagen: ' . pg_last_error());
 	
-	$issue = $_POST['Issue'];
-	$title = $_POST['Title'];
-	$description = $_POST['Description'];
-	$category = $_POST['Category'];
-	$person_contact = $_POST['PersonContact'];
-	$feature = $_POST['Geometry'];
-	$person_name = $_POST['Name'];
-	$people_attending = $_POST['PeopleAttending'];
-	$people_need = $_POST['PeopleNeeded'];
-	$tags = $_POST['Tags'];
-	$creationDate = $_POST['CreationDate'];
+		$issue = $_POST['Issue'];
+		$title = $_POST['Title'];
+		$description =$_POST['Description'];
+		$category = $_POST['Category'];
+		$person_contact = $_POST['PersonContact'];
+		$feature = $_POST['Geometry'];
+		$person_name = $_POST['Name'];
+		$people_attending = $_POST['PeopleAttending'];
+		$people_need = $_POST['PeopleNeeded'];
+		$tags = $_POST['Tags'];
+		$creationDate = $_POST['CreationDate'];
 
-	$feature;
-	$coords;
-	$geometry;
+		$feature;
+		$coords;
+		$geometry;
 
-	
+		$data_send = array(	
+			"issue" => $issue,
+			"title" => $title,
+			"description" => $description,
+			"category" => $category,
+			"person_contact" => $person_contact,
+			"feature" => $feature,
+			"person_name" => $person_name,
+			"person_attending" => $people_attending,
+			"people_need" => $people_need,
+			"tags" => $tags,
+			"creationDate" => $creationDate,	
+		);
+
 	if ($feature != ""){
 	$feature = json_decode($feature);
 	$coords = $feature->geometry->coordinates;
 	$geometry = $feature->geometry->type;
 	}
-	
+
 	function buildCoords($coordinates, $type){
 		$coordString = '(';
 		if ( $type == 'Point' ){
@@ -41,18 +54,18 @@ $dbconn = pg_connect("host=host port=5432 dbname=dbname user=user password=pw")
 			for ( $i = 0; $i < count($coordinates); $i++ ){
 				for ( $j = 0; $j < count($coordinates[$i]); $j++ ){
 					$coordString = $coordString.$coordinates[$i][$j][0].' '.$coordinates[$i][$j][1].',';
-					
+
 				}
 			}
 			$coordString = substr($coordString,0,strlen($coordString)-1);
 			$coordString = $coordString.')';
 		}
-	
+
 		$coordString = $coordString.')';
 		return $coordString;
 	}
 
-	
+
 	if ($feature != ""){
 		$coordinates = buildCoords($coords, $geometry);
 	}
@@ -61,7 +74,7 @@ $dbconn = pg_connect("host=host port=5432 dbname=dbname user=user password=pw")
 		$coordinates = "(0 0)";
 	}
 	$query=pg_query($dbconn,"Insert into \"message\" values (DEFAULT,'".$issue."','".$title."','".$geometry.$coordinates."',TIMESTAMP '".$creationDate."',true,TIMESTAMP '".$creationDate."','".$description."',".$people_need.",".$people_attending.",'','".$category."','','".$person_name."','".$person_contact."','delete?');");
-	
+	echo json_encode ($data_send);
 	//REMEBER: Select ST_AsText(location) from message;
-	
+
 ?>
