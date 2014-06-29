cda1d3223b59b0766f56e282c3034f7840ef8c98
Dominik <schlomm.darmann@googlemail.com>
2014-06-29 20:24:59 +0200

    Updated database schema to current used one
    - Added SRID_ID information
    - Added hulluser_id
    - Removed unused information

--- Database/database.sql
+++ Database/database.sql
@@ -1,27 +1,30 @@
 DROP TABLE comment;
 DROP TABLE message;
 
-CREATE TABLE message(
-message_id SERIAL NOT NULL,
-message_type TEXT NOT NULL,
-title TEXT,
-location GEOMETRY,
-time_start TIMESTAMP NOT NULL,
-relevant BOOLEAN NOT NULL,
-date_of_change TIMESTAMP NOT NULL,
-description TEXT NOT NULL,
-people_needed INT,
-people_attending INT,
-file BYTEA,
-category TEXT,
-tags TEXT,
-person_name TEXT,
-person_contact TEXT,
-person_email TEXT,
-PRIMARY KEY (message_id)
+CREATE TABLE message
+(
+message_id serial NOT NULL,
+message_type text NOT NULL,
+title text,
+location geometry(Geometry,4326),
+time_start timestamp without time zone NOT NULL,
+relevant boolean NOT NULL,
+date_of_change timestamp without time zone NOT NULL,
+description text NOT NULL,
+people_needed integer,
+people_attending integer,
+file bytea,
+category text,
+tags text,
+person_name text,
+person_contact text,
+person_email text,
+hulluser_id text,
+CONSTRAINT message_pkey PRIMARY KEY (message_id)
 );
 
-CREATE TABLE comment(
+CREATE TABLE comment
+(
 comment_id SERIAL NOT NULL,
 message_id INT NOT NULL,
 date_of_creation TIMESTAMP NOT NULL,
