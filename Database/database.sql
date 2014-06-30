1d61f614aa8730002439691398d4bd2623ff485a
froehr <f.roehr@uni-muenster.de>
2014-06-30 17:31:01 +0200

    Database last status

--- Database/database.sql
+++ Database/database.sql
@@ -1,4 +1,3 @@
-DROP TABLE comment;
 DROP TABLE message;
 
 CREATE TABLE message
@@ -13,7 +12,7 @@ date_of_change timestamp without time zone NOT NULL,
 description text NOT NULL,
 people_needed integer,
 people_attending integer,
-file bytea,
+file boolean,
 category text,
 tags text,
 person_name text,
@@ -21,18 +20,4 @@ person_contact text,
 person_email text,
 hulluser_id text,
 CONSTRAINT message_pkey PRIMARY KEY (message_id)
-);
-
-CREATE TABLE comment
-(
-comment_id SERIAL NOT NULL,
-message_id INT NOT NULL,
-date_of_creation TIMESTAMP NOT NULL,
-message TEXT NOT NULL,
-file BYTEA,
-person_name TEXT,
-PRIMARY KEY (comment_id),
-FOREIGN KEY (message_id) REFERENCES message
-	ON DELETE CASCADE
-	ON UPDATE CASCADE
 );
