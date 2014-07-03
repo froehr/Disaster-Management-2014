c6f0563cef5885174fb6cad73b083ea6e56932d6
AxisStarstreamer <andre.wieghardt@gmail.com>
2014-07-03 14:28:21 +0200

    deleting Features
    now the features are not going to be deleted. The relevant is setting to FALSE

--- dm/php/deleteMessages.php
+++ dm/php/deleteMessages.php
@@ -4,7 +4,7 @@ include 'db_connect.php';
 	$con = getConnection();
 	$message_id = $_POST['message_id'];
 
-	$query=pg_query($con,'DELETE FROM "message" WHERE "message_id"='.$message_id.';');
+	$query=pg_query($con,'UPDATE message SET relevant = FALSE WHERE "message_id"='.$message_id.';');
 	echo ("Successfully deleted");
 
 ?>
