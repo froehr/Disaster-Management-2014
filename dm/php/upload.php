252a0f108a37e647f860e4a836738a9d5754f21e
Markus <mkonkol@arcor.de>
2014-07-08 15:18:05 +0200

    save thumbnail (see description)
    for each image uploaded via a message, a thumbnail is created and saved
    in php/upload/thumbnail

--- dm/php/upload.php
+++ dm/php/upload.php
@@ -1,10 +1,23 @@
 <?php
+// see also http://code.tutsplus.com/tutorials/image-resizing-made-easy-with-php--net-10362
+include("resize-class.php");
+
 $id = $_POST["id"];
 if (isset($_FILES['datei']))
 
 {
-     move_uploaded_file($_FILES['datei']['tmp_name'], 'upload/'.basename($_FILES['datei']['name'])');
+     move_uploaded_file($_FILES['datei']['tmp_name'], 'upload/'.basename($_FILES['datei']['name']));
 }
 
-//echo $id;
+//$name -> basename($_FILES['datei']['name']);
+
+// *** 1) Initialize / load image
+$resizeObj = new resize('upload/'.basename($_FILES['datei']['name']));
+ 
+// *** 2) Resize image (options: exact, portrait, landscape, auto, crop)
+$resizeObj -> resizeImage(150, 150, 'landscape');
+
+// *** 3) Save image
+$resizeObj -> saveImage('upload/thumbnail/'.basename($_FILES['datei']['name']), 100);
+
 ?>
